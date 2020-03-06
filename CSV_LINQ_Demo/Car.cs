9b243cd7d90d9dfc20c2d2f6026677d7058e73af
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2020-03-06 07:49:52 +0100

    Added csv LINQ demo

---
+++ CSV_LINQ_Demo/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace CSV_LINQ_Demo
+{
+    public struct Car
+    {
+        public string Manufacturer { get; set; }
+        public int ModelYear { get; set; }
+        public string Division { get; set; }
+        public string Model { get; set; }
+        public double Displacement { get; set; }
+        public int Cylinders { get; set; }
+        public int CombinedFuelEfficiency { get; set; }
+        internal static Car TransformFromString(string line, int arg2)
+        {
+            var columns = line.Split(';');
+            return new Car
+            {
+                ModelYear = int.Parse(columns[0]),
+                Manufacturer = columns[1],
+                Division = columns[2],
+                Model = columns[3],
+                Displacement = double.Parse(columns[4]),
+                Cylinders =  int.Parse(columns[5]),
+                CombinedFuelEfficiency = int.Parse(columns[8])
+            };
+        }
+    }
+}
---
+++ CSV_LINQ_Demo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CSV_LINQ_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessFile("car-data.csv");
+
+            var query = cars.OrderByDescending(c => c.CombinedFuelEfficiency);
+
+            foreach(var car in query.Take(50))
+            {
+                Console.WriteLine(car.Division + " " + car.Model + " " + car.CombinedFuelEfficiency);
+            }
+
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            var result = File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 1)
+                .Select(Car.TransformFromString);
+
+            return result.ToList<Car>();
+        }
+    }
+}
+ 
