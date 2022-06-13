be7ea99c7a6e15533b0e07344da67f8269071687
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2022-06-13 09:09:31 +0200

    Modified NullableDemo for .NET 6/C#10

--- NullableDemo/Program.cs
+++ NullableDemo/Program.cs
@@ -1,38 +1,30 @@
 ﻿using System;
-#nullable enable
-namespace NullableDemo
+
+#nullable enable // if not specified in csproj
+
+var item1 = new Item("1");
+item1.Customer = new Customer("Pelle");
+
+var item2 = new Item("2");
+item2.Customer = new Customer(null);    // Should give warning
+Console.WriteLine($"Customer 2: {item2.Customer.Name}");
+
+Console.WriteLine(item1.Customer?.Name ?? "No customer defined");
+// Should handle null reference
+Console.WriteLine(item2.Customer?.Name ?? "No customer defined");
+
+class Customer
 {
-    class Customer
-    {
-        public string Name { get; set; }
-        public Customer(string name) => Name = name;
-    }
+    public string Name { get; set; }
+    public Customer(string name) => Name = name;
+}
 
-    class Item
-    {
-        public string Id { get; set; }
-        public Customer? Customer { get; set; }
-        public Item(string id) => Id = id;
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            var customer1 = new Customer("Pelle");
-            _ = new Customer(null);    // Should give warning
-
-            var item1 = new Item("1");
-            // Should give warning and throw exception
-            // Console.WriteLine(item1.Customer.Name);
-
-            // Should handle null reference
-            Console.WriteLine(item1.Customer?.Name ?? "No customer defined");
-            item1.Customer = customer1;
-            Console.WriteLine(item1.Customer?.Name ?? "No customer defined");
-        }
-    }
+class Item
+{
+    public string Id { get; set; }
+    public Customer? Customer { get; set; }
+    public Item(string id) => Id = id;
 }
 
 #nullable restore
+
